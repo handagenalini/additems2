ace42fd70c3c5109caf0d44507a4f117bdf12d17
handagenalini <111242818+handagenalini@users.noreply.github.com>
2022-08-15 11:14:44 +0530

    added color and made bold

--- README.md
+++
@@ -1,1 +0,0 @@
-# additems2
---
+++ dom2.js
@@ -0,0 +1,28 @@
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.Title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.All);
+// console.log(document.All[0]);
+// document.All[10].textcontent
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+// var headerTitle=(document.getElementById(header-Title));
+// console.log(hederTitle);
+// headerTitle.textContent='hello';
+// headerTitle.innerText='goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML=<h3>hello</h3>
+// headerTitle.style.borderBottom='solid 3px #000';
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+for (var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+items[1].textContent='hello';
+
+items[2].style.backgroundcolor='green';
