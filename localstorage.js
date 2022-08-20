fa9f37672d878e1a5d7d06c5b8605549896c3cd0
handagenalini <111242818+handagenalini@users.noreply.github.com>
2022-08-20 19:50:19 +0530

    localstorage

--- localstorage.js
+++ localstorage.js
@@ -129,6 +129,28 @@ function filterItems(e){
     }
   });
 }
+<body>
+    <form onsubmit="saveToLocalStorage(event)">
+    <label >Name</label>
+<input type="text" name="username" required/>
+<label >Emailid</label>
+<input type="email" name="emailid" required/>
+<label > Phonenumber</label>
+<input type="tel" name="phonenumber" />
+<button>submit</button>
+</form>
+<script>
+    function saveToLocalStorage(event){
+        event.preventDefault();
+        const name=event.target.username.value;
+        const email=event.target.emailid.value;
+        const phonenumber=event.target.phonenumber.value;
+        localStorage.setItem('name',name);
+        localStorage.setItem('emailid',email);
+        localStorage.setItem('phonenumber',phonenumber);
+    }
+</script>
+</body>
 
 
 
